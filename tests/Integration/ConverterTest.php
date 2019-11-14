c1a48486329a17ac3e3f47d2dd64d2252d2e6f37
Cesar Aguilera <cesargnu29@gmail.com>
2019-11-14 14:49:40 -0600

    Add tests for pdf with payment data

--- tests/Integration/ConverterTest.php
+++ tests/Integration/ConverterTest.php
@@ -39,4 +39,49 @@ class ConverterTest extends TestCase
 
         unlink($created);
     }
+
+    public function testConverterWithPaymentData()
+    {
+        $cfdi = Cfdi::newFromString(Cleaner::staticClean($this->fileContents('cfdi33-payment-valid.xml')));
+
+        $cfdiData = (new CfdiDataBuilder())
+            ->withXmlResolver($this->createXmlResolver())
+            ->build($cfdi->getNode());
+
+        $pagos = $cfdiData->comprobante()->searchNodes('cfdi:Complemento', 'pago10:Pagos', 'pago10:Pago');
+
+        $pago = $pagos->first();
+        if (null === $pago) {
+            $this->fail('Specimen does not have a Pago element');
+            return;
+        }
+
+        $doctosRelacionados = $pago->searchNodes('pago10:DoctoRelacionado');
+
+        $doctoRelacionado = $doctosRelacionados->first();
+        if (null === $doctoRelacionado) {
+            $this->fail('Specimen does not have a DoctoRelacionado element');
+            return;
+        }
+
+        $builder = new Html2PdfBuilder();
+        $converter = new Converter($builder);
+
+        $created = $this->filePath('cfdi33-payment-valid.pdf');
+        $converter->createPdfAs($cfdiData, $created);
+        $this->assertFileExists($created);
+
+        $pdfToString = new PdfToText();
+        $contents = $pdfToString->extract($created);
+        $this->assertContains($doctoRelacionado['IdDocumento'], $contents);
+        $this->assertContains($doctoRelacionado['Serie'], $contents);
+        $this->assertContains($doctoRelacionado['Folio'], $contents);
+        $this->assertContains($doctoRelacionado['MonedaDR'], $contents);
+        $this->assertContains($doctoRelacionado['MetodoDePagoDR'], $contents);
+        $this->assertContains($doctoRelacionado['NumParcialidad'], $contents);
+        $this->assertContains($doctoRelacionado['ImpPagado'], $contents);
+        $this->assertContains($doctoRelacionado['ImpSaldoInsoluto'], $contents);
+        $this->assertContains($doctoRelacionado['ImpSaldoAnt'], $contents);
+        unlink($created);
+    }
 }
